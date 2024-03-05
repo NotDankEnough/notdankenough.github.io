de1d2dedac0c377d1639e1509b95bb1a07a9a0ce
ilotterytea <ilotterytea@proton.me>
2024-03-05 21:10:36 +0500

    upd: moved /under-construction to /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,19 @@
-import Item from '@/components/Item';
-import Navbar from '@/components/Navbar'
+import Image from "next/image";
 
-export default function Home() {
-  const apps = [
-    {
-      "name": "Maxon Petting Simulator",
-      "short": "A silly clicker game about a cute kitty whose cheeks must always be squished.",
-      "image_url": "/maxon.png",
-      "url": "https://ilotterytea.itch.io/maxon"
-    }
-  ];
-
-  return (
-    <main className="flex min-h-screen flex-col items-center">
-      <div className="px-16 w-full space-y-8 xl:space-y-0 xl:w-[70%]">
-        <Navbar />
-        <div className="flex flex-col space-y-8 xl:space-y-0 xl:grid xl:grid-cols-3 xl:gap-4">
-          {
-            apps.map((value, index) => (
-              <Item
-                name={value.name}
-                short={value.short}
-                image_url={value.image_url}
-                url={value.url}
-                key={index}
-              />
-            ))
-          }
-        </div>
-      </div>
-    </main>
-  )
+export default function Page() {
+    return (
+        <main className="flex min-h-screen flex-col items-center">
+            <div className="flex flex-col xl:px-16 w-full min-h-screen xl:w-[70%]">
+                <div className="grow"></div>
+                <div className="flex flex-row-reverse">
+                    <Image
+                        src={"/silly_cat.png"}
+                        width={64}
+                        height={64}
+                        alt="hi"
+                    />
+                </div>
+            </div>
+        </main>
+    );
 }
--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export function middleware(request: NextRequest) {
-    return NextResponse.rewrite(new URL("/under_construction", request.url));
-}
-
-export const config = {
-    matcher: "/"
-}
